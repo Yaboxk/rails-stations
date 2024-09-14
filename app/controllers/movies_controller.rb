0ff02023a35db792596f8cce4f88f8bbb750b0e4
YaboxK <yaboxk@gmail.com>
2024-09-15 00:50:36 +0900

    06

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,12 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+    if params[:keyword].present?
+      @movies = @movies.where('name LIKE ? OR description LIKE ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%")
+    end
+
+    if params[:is_showing].present?
+      @movies = @movies.where(is_showing: params[:is_showing])
+    end
   end
 end
