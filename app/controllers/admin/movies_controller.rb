50beb5b0d554e8b224a40bc99343efb6e661e64e
YaboxK <yaboxk@gmail.com>
2024-09-14 23:31:33 +0900

    04

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -18,6 +18,22 @@ class Admin::MoviesController < ApplicationController
     end
 
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    movie = Movie.find(params[:id])
+    if movie.update(movie_params)
+      redirect_to admin_movies_path
+    else
+      flash.now[:alert] = movie.errors.full_messages.join("")
+      @movie = movie
+      render "edit"
+    end
+  end
+
   
   private
   def movie_params
